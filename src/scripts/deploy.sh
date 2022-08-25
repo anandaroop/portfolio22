e78e654d07f6a930cc51dd4d9a94a6cac9904111
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2022-08-25 00:25:43 -0400

    build: add build + deploy scripts

---
+++ src/scripts/deploy.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# set .env
+if [[ ! -e .env ]]; then
+    echo ".env file is missing"
+    exit 1
+fi
+export $(grep -v '^#' .env | xargs)
+
+# schlep
+tar cvfz - out | ssh ${ARCOM_USER}@${ARCOM_HOST} 'cd ~/www/next; rm -r html.bak; mv html html.bak; tar xvfz -; mv out html'
