4365035a29b5d96e052355e243369addcf23bcb7
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2022-08-25 00:39:47 -0400

    style: container

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,19 @@ import "../../styles/globals.css"
 import type { AppProps } from "next/app"
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <div className="bg-gray-200">
+        <div className="container mx-auto bg-gray-100">
+          <header>Anandaroop Roy</header>
+          <main>
+            <Component {...pageProps} />
+          </main>
+          <footer>Copyright 2000–{new Date().getFullYear()}</footer>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default MyApp
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import Head from "next/head"
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <>
       <Head>
         <title>Portfolio 22</title>
         <meta
@@ -13,14 +13,8 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <header>Anandaroop Roy</header>
-      <main>
-        <div>
-          <h1 className="text-2xl my-4">Portfolio 22</h1>
-        </div>
-      </main>
-      <footer>Copyright 2000–{new Date().getFullYear()}</footer>
-    </div>
+      <h1 className="text-2xl my-4">Portfolio 22</h1>
+    </>
   )
 }
 
