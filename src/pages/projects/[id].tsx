e5ba9e8ffec2f0ad37fc65f0e0effe852c30649d
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2022-08-26 23:22:17 -0400

    feat: enable prose styling on project page

--- src/pages/projects/[id].tsx
+++ src/pages/projects/[id].tsx
@@ -20,10 +20,19 @@ const Page: NextPage<Props> = ({ project }) => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div className="p-5">
-        <h2>{project.title}</h2>
+      <div className="p-6">
+        <h2 className="text-3xl font-medium md:text-4xl lg:text-5xl">
+          {project.title}
+        </h2>
 
-        <div dangerouslySetInnerHTML={{ __html: project.description }} />
+        <div className="my-2 p-1 text-lg font-medium md:text-xl lg:text-2xl">
+          {project.Client.name}, {project.year}
+        </div>
+
+        <div
+          className="maxleading-loose prose prose-neutral my-4 dark:prose-invert dark:text-neutral-400 md:prose-xl lg:prose-2xl"
+          dangerouslySetInnerHTML={{ __html: project.description }}
+        />
 
         <div className="flex flex-wrap">
           {project.Slides.map((slide: Slide) => (
