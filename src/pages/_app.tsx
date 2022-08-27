f9e5c2ff5a6d2620c0e03600ea8c793219962a35
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2022-08-26 23:22:39 -0400

    style: more debugger helpers

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,14 +6,15 @@ import { Footer } from "~/components/Footer"
 const DEBUG_BREAKPOINTS = false
 
 const breakpoint_outline_styles =
-  "outline outline-4 outline-red-500 sm:outline-orange-500 md:outline-yellow-500 lg:outline-green-500 xl:outline-blue-600 2xl:outline-violet-500"
+  "outline outline-8 outline-red-500 sm:outline-orange-500 md:outline-yellow-500 lg:outline-green-500 xl:outline-blue-600 2xl:outline-violet-500"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      {DEBUG_BREAKPOINTS && <Debugger />}
       <div className="min-h-screen bg-neutral-200 text-neutral-900 dark:bg-neutral-700 dark:text-neutral-300">
         <div
-          className={`container mx-auto min-h-screen bg-neutral-100 dark:bg-neutral-700 ${
+          className={`container mx-auto min-h-screen bg-neutral-100 dark:bg-neutral-600 ${
             DEBUG_BREAKPOINTS && breakpoint_outline_styles
           }`}
         >
@@ -29,3 +30,14 @@ function MyApp({ Component, pageProps }: AppProps) {
 }
 
 export default MyApp
+
+const Debugger = () => (
+  <div className="absolute h-[1em] overflow-hidden bg-white bg-opacity-70 text-5xl font-bold [line-height:100%]">
+    <div className="text-red-500 sm:hidden">⦰</div>
+    <div className="text-orange-500 md:hidden">sm</div>
+    <div className="text-yellow-500 lg:hidden">md</div>
+    <div className="text-green-500 xl:hidden">lg</div>
+    <div className="text-blue-500 2xl:hidden">xl</div>
+    <div className="text-violet-500">2xl</div>
+  </div>
+)
