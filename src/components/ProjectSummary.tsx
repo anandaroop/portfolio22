e91d2b2687d33b21bf95a523618cf6cfabaf970d
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2022-08-27 22:01:47 -0400

    style: tweaks for dark mode etc

--- src/components/ProjectSummary.tsx
+++ src/components/ProjectSummary.tsx
@@ -22,20 +22,19 @@ export const ProjectSummary: React.FC<Props> = ({ project }) => {
 
   return (
     <Link href={`/projects/${slug}`}>
-      <a className="group aspect-square w-1/2 p-4 sm:w-1/3 lg:w-1/5">
+      <a className="group aspect-square w-1/2 p-4 sm:w-1/3 lg:w-1/4 xl:w-1/5">
         <div className="relative">
-          <div
-            className="absolute top-0 z-10 m-2 w-auto overflow-hidden overflow-ellipsis p-2 text-lg font-medium text-neutral-700 transition-opacity group-hover:opacity-0 group-focus:opacity-0 sm:text-xl lg:text-2xl"
-            style={{ textShadow: "0 0 5px #fff" }}
-          >
-            <h2 className="text-neutral-900">{project.title}</h2>
-            <div className="font-normal text-neutral-500">
+          <div className="text-shadow dark:text-shadow-dark absolute top-0 z-10 w-full overflow-hidden overflow-ellipsis p-2 text-lg font-medium  text-neutral-700 transition-opacity group-hover:opacity-0 group-focus:opacity-0 md:p-4 md:text-xl lg:text-2xl">
+            <h2 className="font-semibold text-neutral-900 dark:text-neutral-100">
+              {project.title}
+            </h2>
+            <div className="hidden font-medium text-neutral-600 dark:text-neutral-300 sm:block">
               {project.Client.name}
             </div>
           </div>
           <LazyLoadImage
             alt={slide.caption}
-            className="w-full opacity-40 transition-opacity group-hover:opacity-100 group-hover:blur-none group-focus:opacity-100 group-focus:blur-none"
+            className="w-full opacity-50 transition-opacity group-hover:opacity-100 group-hover:blur-none group-focus:opacity-100 group-focus:blur-none dark:opacity-60"
             wrapperClassName="w-full aspect-square"
             src={imgSrc}
             placeholderSrc={slide.placeholder}
