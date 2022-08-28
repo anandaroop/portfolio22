e5ff0416b08f266ad8b7b4d3cba0e16450ef6bb1
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2022-08-28 16:04:50 -0400

    style: header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,14 +3,16 @@ import Link from "next/link"
 export const Header = () => (
   <Link href={`/`}>
     <a className="flex-initial">
-      <header className="border-b-2 border-b-neutral-200 px-6 pt-5 pb-4 opacity-70 hover:opacity-100 dark:border-b-neutral-700">
-        <h1 className="inline-block text-2xl dark:text-white">
-          Anandaroop Roy
-          <span className="ml-2 text-neutral-500 dark:text-[rgb(180,180,180)]">
-            cartography
-          </span>
-          <span className="ml-2 text-neutral-400 dark:text-[rgb(150,150,150)]">
-            & information design
+      <header className="border-b-2 border-b-neutral-200 px-6 py-5 opacity-70 hover:opacity-100 dark:border-b-neutral-700 lg:mb-2 lg:pb-8">
+        <h1 className="flex flex-col text-xl dark:text-white lg:block lg:text-3xl">
+          <span>Anandaroop Roy</span>
+          <span className="text-lg lg:ml-4 lg:text-3xl">
+            <span className="text-neutral-500 dark:text-[rgb(180,180,180)]">
+              cartography&nbsp;
+            </span>
+            <span className="text-neutral-400 dark:text-[rgb(150,150,150)]">
+              & information design
+            </span>
           </span>
         </h1>
       </header>
