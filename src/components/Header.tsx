d5f5cf695a92325d85a06f6c9d04ab33686749cb
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2022-08-30 01:12:00 -0400

    feat: header ui for search

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,21 +1,35 @@
 import Link from "next/link"
 
 export const Header = () => (
-  <Link href={`/`}>
-    <a className="flex-initial">
-      <header className="border-b-2 border-b-neutral-200 px-6 py-5 opacity-70 hover:opacity-100 dark:border-b-neutral-700 lg:mb-2 lg:pb-8">
-        <h1 className="flex flex-col text-xl dark:text-white lg:block lg:text-3xl">
-          <span>Anandaroop Roy</span>
-          <span className="text-lg lg:ml-4 lg:text-3xl">
-            <span className="text-neutral-500 dark:text-[rgb(180,180,180)]">
-              cartography&nbsp;
-            </span>
-            <span className="text-neutral-400 dark:text-[rgb(150,150,150)]">
-              & information design
-            </span>
+  <div className="flex-initial">
+    <header className="flex content-between border-b-2 border-b-neutral-200 px-6 py-5 opacity-70 hover:opacity-100 dark:border-b-neutral-700 lg:mb-2 lg:pb-8">
+      <div className="flex-1 basis-3/4">
+        <Link href={`/`}>
+          <a className="inline-block">
+            <h1 className="flex flex-col text-xl dark:text-white lg:block lg:text-3xl">
+              <span className="">Anandaroop Roy</span>
+              <span className="text-lg lg:ml-4 lg:text-3xl">
+                <span className="text-neutral-500 dark:text-[rgb(180,180,180)]">
+                  cartography&nbsp;
+                </span>
+                <span className="text-neutral-400 dark:text-[rgb(150,150,150)]">
+                  & information design
+                </span>
+              </span>
+            </h1>
+          </a>
+        </Link>
+      </div>
+      <div className="flex-0 ml-2 flex flex-col justify-center">
+        <button className="border-1 border-neutral-300 bg-white py-1 px-2 placeholder-neutral-200 dark:bg-neutral-500 dark:text-neutral-100 xl:px-4 xl:py-2">
+          <span>Search&nbsp;</span>
+          <span className="hidden xl:inline">
+            by keyword, client or year&nbsp;
           </span>
-        </h1>
-      </header>
-    </a>
-  </Link>
+          <span className="hidden sm:inline">🔎</span>
+        </button>
+        {/* <div>lol</div> */}
+      </div>
+    </header>
+  </div>
 )
