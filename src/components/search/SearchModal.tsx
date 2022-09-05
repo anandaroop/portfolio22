de5aa6466b40c7bea5420db757a4316f2dbc10b4
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2022-09-05 00:26:20 -0400

    style: responsive search modal

--- src/components/search/SearchModal.tsx
+++ src/components/search/SearchModal.tsx
@@ -54,13 +54,13 @@ export const SearchModal: React.FC = () => {
       className="modal-styles container h-[90vh] w-[60em] border-8 border-black border-opacity-20 bg-white bg-opacity-95 p-7"
       ref={dialogRef}
     >
-      <div className="text-xl">
-        <h2 className="mb-4 text-2xl font-bold">
+      <div className="lg:text-xl">
+        <h2 className="mb-4 text-base font-bold lg:text-2xl">
           Search results{query.length > 1 ? ` for "${query}"` : ""}
         </h2>
 
         <input
-          className="my-2 w-full border-2 border-neutral-200 p-1 text-xl"
+          className="my-2 w-full border-2 border-neutral-200 px-2 py-1 text-lg lg:px-3 lg:py-2 lg:text-xl"
           type="text"
           placeholder="Enter some search terms to see results"
           value={query}
@@ -82,9 +82,9 @@ const MatchingTags: React.FC<{ tags: Partial<TagDocument>[] }> = ({ tags }) => {
   if (!tags.length) return null
 
   return (
-    <div>
-      <h3 className="mt-5 mb-2 font-bold">Tags</h3>
-      <ul className="leading-8">
+    <div className="pt-1 first-of-type:pt-0 lg:pt-2">
+      <h3 className="mt-2 font-bold lg:mt-4">Tags</h3>
+      <ul className="leading-7 lg:leading-9">
         {tags.map((tag) => {
           return (
             <li className="mr-2 inline-block" key={tag.id}>
@@ -107,9 +107,9 @@ const MatchingClients: React.FC<{ clients: Partial<ClientDocument>[] }> = ({
   if (!clients.length) return null
 
   return (
-    <div>
-      <h3 className="mt-5 mb-2 font-bold">Clients</h3>
-      <ul className="leading-8">
+    <div className="pt-1 first-of-type:pt-0 lg:pt-2">
+      <h3 className="mt-2 font-bold lg:mt-4">Clients</h3>
+      <ul className="leading-7 lg:leading-9">
         {clients.map((client) => {
           return (
             <li className="mr-2 inline-block" key={client.id}>
@@ -133,15 +133,18 @@ const MatchingProjects: React.FC<{ projects: ProjectDocument[] }> = ({
   if (!query.length) return null
 
   return (
-    <div className="">
-      <h3 className="mt-5 mb-2 font-bold">Projects</h3>
+    <div className="pt-1 first-of-type:pt-0 lg:pt-2">
+      <h3 className="my-2 font-bold lg:my-4">Projects</h3>
       <ul>
         {projects.map((project: ProjectDocument) => {
           const slug = getSlugFromProject(project as unknown as Project)
           return (
             <li className="mr-2 inline" key={project.id}>
               <Link href={`/projects/${slug}`}>
-                <a className="text-neutral-500 " onClick={exitSearchMode}>
+                <a
+                  className="text-sm text-neutral-500 lg:text-xl"
+                  onClick={exitSearchMode}
+                >
                   <div className="flex items-center">
                     <img
                       className="mr-4 h-12"
