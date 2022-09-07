636f39a8ad99f77653451708944e55930b9fab7d
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2022-09-06 21:17:14 -0400

    feat: add clarity analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,30 +6,46 @@ import { Header } from "~/components/Header"
 import { Footer } from "~/components/Footer"
 import { SearchProvider } from "~/components/search/SearchContext"
 import { SearchModal } from "~/components/search/SearchModal"
+import Script from "next/script"
 
 const breakpoint_outline_styles =
   "outline outline-8 outline-red-500 sm:outline-orange-500 md:outline-yellow-500 lg:outline-green-500 xl:outline-blue-600 2xl:outline-violet-500"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <SearchProvider>
-      {/* <SearchDebug /> */}
-      <SearchModal />
-      {DEBUG_BREAKPOINTS && <Debugger />}
-      <div className="min-h-screen bg-neutral-200 text-neutral-900 dark:bg-neutral-700 dark:text-neutral-300">
-        <div
-          className={`container mx-auto flex min-h-screen flex-col bg-neutral-100 dark:bg-neutral-600 ${
-            DEBUG_BREAKPOINTS && breakpoint_outline_styles
-          }`}
-        >
-          <Header />
-          <main className="flex-1">
-            <Component {...pageProps} />
-          </main>
-          <Footer />
+    <>
+      <Script
+        id="clarity-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "djn44nn84e");
+          `,
+        }}
+      />
+      <SearchProvider>
+        {/* <SearchDebug /> */}
+        <SearchModal />
+        {DEBUG_BREAKPOINTS && <Debugger />}
+        <div className="min-h-screen bg-neutral-200 text-neutral-900 dark:bg-neutral-700 dark:text-neutral-300">
+          <div
+            className={`container mx-auto flex min-h-screen flex-col bg-neutral-100 dark:bg-neutral-600 ${
+              DEBUG_BREAKPOINTS && breakpoint_outline_styles
+            }`}
+          >
+            <Header />
+            <main className="flex-1">
+              <Component {...pageProps} />
+            </main>
+            <Footer />
+          </div>
         </div>
-      </div>
-    </SearchProvider>
+      </SearchProvider>
+    </>
   )
 }
 
