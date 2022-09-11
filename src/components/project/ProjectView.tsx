fa30b8c9ef7e7b78c79a7958c42baf93aae15a2d
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2022-09-11 09:51:23 -0400

    refactor: extract project view

---
+++ src/components/project/ProjectView.tsx
@@ -0,0 +1,47 @@
+import { Project } from "~/types"
+
+import Link from "next/link"
+import { getSlugFromClient } from "~/lib/slugs"
+import { SlideShow } from "./SlideShow"
+import { VimeoClips } from "./VimeoClips"
+
+interface Props {
+  project: Project
+}
+
+export const ProjectView: React.FC<Props> = ({ project }) => {
+  const clientSlug = getSlugFromClient(project.Client)
+  const photoSlides = project.Slides.filter((slide) => !slide.clip)
+  const videoClips = project.Slides.filter((slide) => !!slide.clip)
+
+  return (
+    <div className="p-6">
+      <h2 className="text-3xl font-medium md:text-4xl lg:text-5xl">
+        {project.title}
+      </h2>
+      {project.subtitle && (
+        <h3 className="my-2 text-lg text-neutral-500 dark:text-neutral-400 md:text-xl lg:text-2xl">
+          {project.subtitle}
+        </h3>
+      )}
+
+      <div className="my-2 text-lg md:text-xl lg:text-2xl">
+        <Link href={`/clients/${clientSlug}`}>
+          <a className="underline-offset-2 hover:underline">
+            {project.Client.name}
+          </a>
+        </Link>
+        , {project.year}
+      </div>
+
+      <div
+        className="prose prose-neutral mt-6 leading-loose text-neutral-500 dark:prose-invert dark:text-neutral-400 md:prose-xl lg:prose-2xl"
+        dangerouslySetInnerHTML={{ __html: project.description }}
+      />
+
+      <SlideShow slides={photoSlides} square />
+
+      {videoClips.length > 0 && <VimeoClips clips={videoClips} />}
+    </div>
+  )
+}
